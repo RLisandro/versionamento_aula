f83f6c56256fc6a326fac67c1e9c4e43ef7af0cd
RLisandro <rezendecazuza@gmail.com>
2025-03-30 10:13:38 -0300

    código calcular média

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def calcular_media(numeros):
+    if not numeros:
+        return 0
+    soma = sum(numeros)
+    media = soma / len(numeros)
+    return media
+
+# Teste da função
+numeros = [10, 20, 30, 40, 50]
+print(f"A média é: {calcular_media(numeros)}")
